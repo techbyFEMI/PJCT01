9718f5c720f4ed9e35029ac5a9a2a655493c6fdb
FEMI <techbyfemi@gmail.com>
2026-03-04 21:51:39 +0100

    Risk score logic added

--- app.py
+++ app.py
@@ -40,12 +40,71 @@ def get_username(User:username):
     return f"Hello, {User.name} I would need to collect some information about you to give you the best advice on how to take care of your health. Please provide me with the following information"
 
 @app.post("/medicinfoept")
-def get_medicinfo(medicinfoAdd:medicinfo):
+def get_medicinfo(medicinfoAdd:medicinfo) ->dict:
+    score =0
     bmi=calculate_BMI(medicinfoAdd.weight, medicinfoAdd.height)
-    return{
-     "bmi": bmi,
-     
-}
+    if bmi < 18.5:
+         score +=1
+    elif 18.5 <= bmi < 24.9:
+            score +=0
+    elif 25 <= bmi < 29.9:
+            score +=2
+    else:        
+          score +=4 
+
+    if medicinfoAdd.smokeRate == SmokeRate.Frequently:
+        score +=3
+    elif medicinfoAdd.smokeRate == SmokeRate.Moderate:
+            score +=1
+    elif medicinfoAdd.smokeRate == SmokeRate.Never:
+          score +=0
+
+    if medicinfoAdd.bloodPressure < 120:
+          score +=0
+    elif 120 <= medicinfoAdd.bloodPressure < 130:
+            score +=1
+    elif 130 <= medicinfoAdd.bloodPressure < 139:
+            score +=2      
+    else:
+          score +=4
+
+    if 60 <= medicinfoAdd.heartRate <= 100:
+            score +=0    
+    elif medicinfoAdd.heartRate> 100:
+            score +=2
+    else:
+          score +=1
+
+    if medicinfoAdd.age < 30:
+            score +=0
+    elif 30 <= medicinfoAdd.age < 44:
+          score +=1
+    elif 45<= medicinfoAdd.age < 59:
+          score +=2
+    else:
+          score +=3
+
+    if medicinfoAdd.existingConditions:
+        score += len(medicinfoAdd.existingConditions) * 2
+    if medicinfoAdd.bodyTemperature > 37.5:
+        score +=1   
+    
+    if score >=10:
+            risk_level = "High Risk"
+    elif score >=5:
+            risk_level = "Moderate Risk"
+    else:
+              risk_level = "Low Risk"
+
+    return {
+                "score": score,
+                "risk_level": risk_level,
+                "disclaimer": "This is a general health risk indicator based on WHO primary care screening guidelines, not a medical diagnosis. Please consult a healthcare professional"
+
+              }
+        
+
+        
 
 
 
